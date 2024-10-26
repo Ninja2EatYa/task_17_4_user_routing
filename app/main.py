e45a57d66a13aef43ce7f30cd77afde83bcb25e9
Holden <777jamesholden@gmail.com>
2024-10-26 20:22:30 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routers.task import router as tr
+from routers.user import router as ur
+
+app = FastAPI(swagger_ui_parameters={'tryItOutEnabled': True})
+
+
+@app.get('/')
+async def welcome() -> dict:
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(ur)
+app.include_router(tr)
